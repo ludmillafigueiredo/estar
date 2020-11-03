91b562e0c59c06cc4402ff02be6e0e906d915e12
ludmillfaigueiredo <ludmillafi@gmail.com>
2020-11-03 10:07:01 +0100

    Feat: Mock time series data of baseline and response state variable

---
+++ R/data.R
@@ -0,0 +1,28 @@
+#' Time-series of mock state variable
+#'
+#' A dataset containing the time-series of a mock state variable
+#' following a Uniform distribution.
+#' Simplest possible example of dataset to be used during early
+#' stages of development.
+#'
+#' @format A data frame with 100 rows and 2 variables:
+#' \describe{
+#'   \item{sv}{state variable}
+#'   \item{t}{time}
+#' }
+#' @source \code{dataframe(sv = runif(n = 100), t = seq(1,100))}
+"mock_svts"
+
+#'
+#' A dataset containing the baseline time-series of a mock state variable
+#' following a Uniform distribution, limited to [0,0.5].
+#' Simplest possible example of dataset to be used during early
+#' stages of development.
+#'
+#' @format A data frame with 100 rows and 2 variables:
+#' \describe{
+#'   \item{sv}{state variable}
+#'   \item{t}{time}
+#' }
+#' @source \code{dataframe(sv = runif(n = 100, min = 0, max = 0.5), t = seq(1,100))}
+"mock_blts"
