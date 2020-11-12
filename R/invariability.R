cc2c503f9f63ef643ca9cea7eb13f54d378dcaea
ludmillfaigueiredo <ludmillafi@gmail.com>
2020-11-12 17:59:56 +0100

    Fix: invariability() passes devtools::check()

--- R/invariability.R
+++ R/invariability.R
@@ -1,54 +1,122 @@
-#' Calculate the invariability of state variable after disturbance.
+#' Calculate the invariability of a state variable after disturbance.
 #'
 #' @description Return the inverse of coefficient of variation or
 #' that of the standard-deviation of residuals of the linear model
 #' derived for the ratio between response over time.
 #'
-#' @param svts_path Path to the state variable time series for which
-#' invariability should be calculated.
-#' @param variant String stating whether invariability should be calculated
-#' as the inverse of the coefficient of variation of the state variable
-#' \code{"cv"}, or as the residual of the linear model derived for the log
-#' response ratio between the response and a baseline.
-#' @param time_frame First and last time steps defining the time frame for which
-#'   invariability should be calculated from the baseline should be looked for.
-#' @param bl_path Path to the state variable time series to be used as baseline.
+#' @param sv_resp a vector containing the response state variable or a string specifying
+#' the name of the column containing said variable in the dataframe provided
+#' in \code{data}.
+#' @param t_resp a vector containing the time or a string specifying the
+#' name of the column containing the time in the dataframe provided
+#' in \code{data}.
+#' @param mode a string stating whether invariability should be calculated
+#' from the coefficient of variation of the state variable \code{mode = "cv"},
+#' or from the linear model between the response and time \code{"lm_res"}.
+#' The details of the two modes as explained in 'Details'.
+#' @param tf_resp a vector, specifying the positional interval of \code{sv} values
+#' from which invariability should be calculated, or the specific time values defining
+#' this interval, if \code{t} is provided.
+#' @param data_resp an optional data frame containing the columns storing the
+#' response state variable and time.
+#' @param sv_bl a vector containing the baseline, or a string containing
+#' the name of the column in \code{data_bl} containing the baseline.
+#' Obligatory argument if \code{mode = "lm_res"}.
+#' @param t_bl an optional vector containing the time steps for which the baseline
+#' was measured, or a string containing the name of the column in \code{data_bl}.
+#' Obligatory argument if \code{mode = "lm_res"}.
+#' @param tf_bl a vector, specifying the positional interval of \code{sv_bl} values
+#' from which invariability should be calculated, or the specific time values defining
+#' this interval, if \code{data_bl} is provided.
+#' Obligatory argument if \code{mode = "lm_res"}.
+#' @param data_bl an optional data frame containing the columns storing the baseline
+#' of the state variable.
+#'
+#' @return a double, the invariability value.
+#'
+#' @examples
+#' invariability(sv_resp = "stat_var", t_resp = "time", mode = "cv",
+#' tf_resp = c(11, 50), data_resp = toy_svts)
+#' invariability(sv_resp = toy_svts$stat_var, t_resp = toy_svts$time, mode = "cv",
+#' tf_resp = c(11, 50))
+#' invariability(sv_resp = "stat_var", t_resp = "time", mode = "lm_res",
+#' tf_resp = c(11, 50), data_resp = toy_svts, sv_bl = "stat_var",
+#' t_bl = "time", tf_bl = c(11, 50), data_bl = toy_blts)
+#' invariability(sv_resp = toy_svts$stat_var, t_resp = toy_svts$time,
+#' tf_resp = c(11, 50), mode = "lm_res", sv_bl = toy_blts$stat_var,
+#' t_bl = toy_blts$time, tf_bl = c(11, 50))
+#'
+#' @details
+#' Invariance can be calculated as the inverse of coefficient of variation
+#' (\code{mode = "cv"} or as the inverse of the residuals of the linear model derived
+#' for the log response ratio between the state variable and the baseline.
+#' @export
+invariability <- function(sv_resp, t_resp , mode, tf_resp, data_resp = NULL,
+                          sv_bl = NULL, t_bl = NULL, tf_bl = NULL, data_bl = NULL){
 
-invariability <- function(svts_path, variant, time_frame, bl_path){
+    if(is.null(data_resp)){
 
-    svts_df <- readr::read_csv(svts_path,  ## RV: important: pay attention to the namespace! + try to use as few dependencies as possible, so I would here just use base function read.csv
-                        col_names = TRUE,
-                        col_types = readr::cols(sv = readr::col_double(), t = readr::col_integer()))  ## RV: this necessitates that the user has named the variables as
-                                                                                 ## sv and time as t. I think I would rather not include path as an argument
-                                                                                 ## and instead have directly the vector of sv as an input (+ time vector)
-                                                                                 ## I think this will allow for more flexibility, as the user can call his variable the
-                                                                                 ## way s/he likes and s/he just has ot supply it as an argument.
-                                                                                 ## I do not think that it is really needed that the data are stored externally,
-                                                                                 ## the user should be able to read-in the data on his own + prepare if needed
+        data_resp <- data.frame(sv_resp = sv_resp, t_resp = t_resp)
 
+    }else{
 
-    if(variant == "cv"){
+      data_resp <- dplyr::rename(data_resp, c("sv_resp" = "stat_var", "t_resp" = "time"))
 
-        invar <- 1/(sd(svts_df$sv)/mean(svts_df$sv))  ## VR: this implies that the whole time series is used to caclulate mean and SD. But it is possible that the
-        ## disturbance is applied at timestep = 5 (or whatever, X), so that the first X time steps have to be discarded when calculating the invariability
-        ## + what about current formulations if there are NAs in the time series???
+    }
+
+    data_resp <- data_resp %>%
+        dplyr::select(sv_resp, t_resp) %>%
+        dplyr::filter(t_resp >= min(t_resp), t_resp <= max(t_resp))
+
+    if(mode == "cv"){
+
+        sv_vct <- dplyr::pull(data_resp, sv_resp)
+
+        if(any(is.na(sv_vct))){
+
+          message("NAs detected among the entries of the state variable")
+
+          if(sum(!is.na(sv_vct)) < 10){
+
+            warning("Less than 10 data points are available for measuring invariability.")
+
+          }
+        }
+
+        invar <- 1/(stats::sd(sv_vct, na.rm = TRUE)/mean(sv_vct, na.rm = TRUE))
 
         return(invar)
 
     }else{
 
-        bl_df <- read_csv(bl_path,
-                          col_names = TRUE,
-                          col_types = cols(sv = col_double(), t = col_integer())) %>%
-            rename(sv = sv_bl)
+        if(mode == "lm_res"){
 
-        invar_df <- dplyr::inner_join(svts_df, bl_df) %>%
-            dplyr::filter(t %in% seq(time_frame)) %>%
-            dplyr::mutate(lrr = log(sv/sv_bl))
+            if(is.null(data_bl)){
 
-        invar <- 1/sd(stats::lm(lrr~t)$residuals)
+                data_bl <- data.frame(sv_bl = sv_bl, t_bl = t_bl)
 
-        return(invar)
+            }else{
+
+              data_bl <- dplyr::rename(data_bl, c("sv_bl" = "stat_var", "t_bl" = "time"))
+
+            }
+
+            data_bl <- data_bl %>%
+              dplyr::filter(t_bl >= min(tf_bl), t_bl <= max(tf_bl)) %>%
+              dplyr::select(sv_bl, t = t_bl)
+
+            data_resp <- dplyr::rename(data_resp, t = t_resp)
+
+            invar_df <- dplyr::inner_join(data_resp, data_bl, by = "t") %>%
+                dplyr::mutate(lrr = log(sv_resp/sv_bl))
+
+            invar <- 1/stats::sd(stats::lm(invar_df$lrr~invar_df$t)$residuals)
+
+            return(invar)
+
+        }else{
+
+            stop("Specify mode of invariability to calculate:\n\"cv\" or \"lm_res\"")}
 
     }
 }
