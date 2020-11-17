dca9345fc4be709f60ac9c17da96931247fdfe84
ludmillfaigueiredo <ludmillafi@gmail.com>
2020-11-17 17:03:28 +0100

    Docs: Obsolete argument

--- R/invariability.R
+++ R/invariability.R
@@ -17,8 +17,8 @@
 #' or from the linear model between the response and time \code{"lm_res"}.
 #' The details of the two modes as explained in 'Details'.
 #' @param tf_resp a numeric vector, specifying the beginning and end of the
-#' interval of \code{sv} values, from which invariability should be calculated,
-#' or the specific time values defining this interval, if \code{t} is provided.
+#' interval of \code{sv_resp} values, from which invariability should be calculated,
+#' or the specific time values defining this interval.
 #' @param na_rm a logical indicating whether NA values should be removed before processing,
 #' defaults to TRUE.
 #' @param data_resp an optional data frame containing the columns storing the
