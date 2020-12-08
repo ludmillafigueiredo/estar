caa857096894439d303968cde0c4894062127492
ludmillfaigueiredo <ludmillafi@gmail.com>
2020-12-08 06:38:49 +0100

    Feat: invariability() calculates all combinations of response and mode

---
+++ R/cv.R
@@ -0,0 +1,10 @@
+#' Calculate coefficient of variation of a vector
+#'
+#' @param vct a numerical vector, for which coefficient of variation should be
+#' calculated.
+#' @param na_rm a logical indicating whether NA values should be removed before
+#' processing.
+#' @noRd
+cv <- function(vct, na_rm) {
+  stats::sd(vct, na.rm = na_rm) / mean(vct, na.rm = na_rm)
+}
--- R/invariability.R
+++ R/invariability.R
@@ -18,6 +18,10 @@
 #' from the coefficient of variation of the state variable \code{mode = "cv"},
 #' or from the linear model between the response and time \code{"lm_res"}.
 #' The details of the two modes as explained in 'Details'.
+#' @param response a string stating whether invariability should be calculated
+#' from the log-ratio response between the values in the disturbed scenario and
+#' the baseline (\code{response = "lrr"}) or for the values in the disturbed
+#' scenario alone.
 #' @param tf_invar a numeric vector, specifying the beginning and end of the
 #' interval of \code{svdb_i} values, from which invariability should be calculated,
 #' or the specific time values defining this interval.
@@ -43,30 +47,30 @@
 #'
 #' @examples
 #' invariability(
-#'   svdb_i = "stat_var", tdb_i = "time", mode = "cv",
+#'   svdb_i = "stat_var", tdb_i = "time", response = "sv", mode = "cv",
 #'   tf_invar = c(11, 50), db_data = toy_dbts
 #' )
 #' invariability(
-#'   svdb_i = toy_dbts$stat_var, tdb_i = toy_dbts$time, mode = "cv",
-#'   tf_invar = c(11, 50)
+#'   svdb_i = toy_dbts$stat_var, tdb_i = toy_dbts$time, response = "sv",
+#'   mode = "cv", tf_invar = c(11, 50)
 #' )
 #' invariability(
-#'   svdb_i = "stat_var", tdb_i = "time", mode = "lm_res",
+#'   svdb_i = "stat_var", tdb_i = "time", response = "lrr", mode = "lm_res",
 #'   tf_invar = c(11, 50), db_data = toy_dbts, svbl_i = "stat_var",
 #'   tbl_i = "time", blinvar_tf = c(11, 50), bl_data = toy_blts
 #' )
 #' invariability(
-#'   svdb_i = toy_dbts$stat_var, tdb_i = toy_dbts$time,
+#'   svdb_i = toy_dbts$stat_var, tdb_i = toy_dbts$time, response = "lrr",
 #'   tf_invar = c(11, 50), mode = "lm_res", svbl_i = toy_blts$stat_var,
 #'   tbl_i = toy_blts$time, blinvar_tf = c(11, 50)
 #' )
 #' @details
 #' Invariance can be calculated as the inverse of the coefficient of variation
 #' (\code{mode = "cv"} or as the standard deviation of the residuals of the linear model
-#' with the predictor being the time and the response being the log response ratio
-#' of the state variable and the baseline.
+#' with the predictor being the time and the response being the state variable or the
+#' log response ratio of the state variable and the baseline (\code{mode = "lm_res"}).
 #' @export
-invariability <- function(svdb_i, tdb_i, mode, tf_invar, db_data = NULL,
+invariability <- function(svdb_i, tdb_i, mode, tf_invar, db_data = NULL, response,
                           svbl_i = NULL, tbl_i = NULL, blinvar_tf = NULL, bl_data = NULL,
                           na_rm = TRUE) {
   dbts_df <- format_input(input = "db", svdb_i, tdb_i, db_data)
@@ -74,38 +78,42 @@ invariability <- function(svdb_i, tdb_i, mode, tf_invar, db_data = NULL,
   dbts_df <- dbts_df %>%
     dplyr::filter(tdb_c >= min(tf_invar), tdb_c <= max(tf_invar))
 
-  if (mode == "cv") {
-    sv_v <- dplyr::pull(dbts_df, svdb_c)
-
-    if (any(is.na(sv_v))) {
-      warning("NAs detected among the entries of the state variable")
-
-      if (sum(!is.na(sv_v)) < 10) {
-        warning("Less than 10 data points are available for measuring invariability.")
-      }
-    }
-
-    invar <- 1 / (stats::sd(sv_v, na.rm = na_rm) / mean(sv_v, na.rm = na_rm))
-
-    return(invar)
+  if (response == "sv") {
+    invar_df <- dplyr::rename(dbts_df,
+      "response" = svdb_c,
+      "t" = tdb_c
+    )
   } else {
-    if (is.null(svbl_i) | is.null(tbl_i)) {
-      stop("Missing 'svbl_i' or 'tbl_i' argument(s).")
-    }
-    if (mode == "lm_res") {
-      blts_df <- format_input(input = "bl", svbl_i, tbl_i, bl_data)
-
-      blts_df <- blts_df %>%
+    if (response == "lrr") {
+      blts_df <- format_input(input = "bl", svbl_i, tbl_i, bl_data) %>%
         dplyr::filter(tbl_c >= min(blinvar_tf), tbl_c <= max(blinvar_tf))
 
       invar_df <- dplyr::inner_join(
-                             dplyr::rename(dbts_df, "t" = tdb_c),
-                             dplyr::rename(blts_df, "t" = tbl_c),
-                             by = "t"
-                         ) %>%
-        dplyr::mutate(lrr = log(svdb_c / svbl_c))
+        dplyr::rename(dbts_df, "t" = tdb_c),
+        dplyr::rename(blts_df, "t" = tbl_c),
+        by = "t"
+      ) %>%
+        dplyr::mutate("response" = log(svdb_c / svbl_c))
+    } else {
+      stop("'response' argument must be \"cv\" or \"lrr\".")
+    }
+  }
 
-      invar <- 1 / stats::sd(stats::lm(invar_df$lrr ~ invar_df$t)$residuals)
+  if (any(is.na(invar_df$response))) {
+    warning("NAs detected among the entries of the state variable")
+
+    if (sum(!is.na(invar_df$response)) < 10) {
+      warning("Less than 10 data points are available for measuring invariability.")
+    }
+  }
+
+  if (mode == "cv") {
+    invar <- 1 / eStar::cv(invar_df$response, na_rm = na_rm)
+
+    return(invar)
+  } else {
+    if (mode == "lm_res") {
+      invar <- 1 / stats::sd(stats::lm(invar_df$response ~ invar_df$t)$residuals)
 
       return(invar)
     } else {
