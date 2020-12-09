8213bd2029940f2d0032136ec34cb861ef51ef9d
ludmillfaigueiredo <ludmillafi@gmail.com>
2020-12-09 20:10:51 +0100

    Fix: interval of persistence separately defined from baseline

--- R/persistence.R
+++ R/persistence.R
@@ -1,7 +1,9 @@
-#' Calculate the persistance of a state variable inside a zone
+#' Calculate the persistance of a state variable inside a defined interval
 #'
-#' @description Return the number of time steps the state variable remained
-#' inside
+#' @description Return the proportion of time the state variable remained
+#' inside the interval defined by the baseline's mean \eqn{\pm} sd. The proportion
+#' is calculated in relation to the time frame for which persistence should be
+#' calculated.
 #'
 #' @param svdb_i a numeric vector containing the state variable in the
 #' disturbed system or a string specifying the name of the column
@@ -13,53 +15,87 @@
 #' of the state variable in a state considered to be disturbed.
 #' @param perst_tf a numerical vector, specifying the beginning and end of the
 #' time interval during which persistence should be measured.
+#' @param bl a string stating whether the baseline is defined by a separate
+#' baseline that is input (\code{bl = "input"}) or by a certain time period of
+#' the disturbed system (\code{bl = "db"}).
+#' @param bl_tf a numerical vector, specifying the beginning and end of the
+#' time interval that defines the baseline (either in the disturbed time-series,
+#' or in the baseline input). See Details.
 #' @param svbl_i a numeric vector containing the state variable in the baseline,
 #' or a string for the name of the column in \code{bl_data} containing said
 #' variable in the baseline.
-#' Obligatory argument if \code{response = "lrr"}.  ## V: param response still to be added
+#' Obligatory argument if \code{bl = "bl"}.
 #' @param tbl_i an optional numeric vector containing the time steps for which
 #' the baseline was measured, or a string containing the name of the column in
 #' \code{bl_data}.
-#' Obligatory argument if \code{response = "lrr"}.
+#' Obligatory argument if \code{bl = "bl"}.
 #' @param bl_data an optional data frame containing the columns storing the baseline
 #' of the state variable.
 #' @param na_rm a logical determining whether NAs should be taken out for the
 #' estimation of variation
 #'
-#' @return a double, the proportion of \code{perst_tf} during which the system persisted
-#'
-#' @details Persistence is defined here as the proportion of time, in relation to a
-#' user-defined time frame, during which the response value (eith state variable or
-#' the log-ratio to baseline) stays inside the interval define by the mean and standard
-#' deviation of the response value during the user-defined time frame.
+#' @return a double, contained in \[0,1\]
+#' 
+#' @details If the baseline is defined by values of the state variable along the
+#' disturbed time-series (\code{bl = "db"}), the time frame used as baseline
+#' (\code{bl_tf}) cannot overlap with the time frame for which the persistence
+#' is to be calculated (\code{perst_tf}), because of redundancy: the values over
+#' \code{bl_tf} define the interval for which the values in \code{perst_tf} are
+#' checked. If they are the same (or partly, if overlap is partial), the
+#' returned value of persistence will be falsely higher.
+#' 
 #' @examples
 #' persistence(
-#'   svdb_i = "stat_var", tdb_i = "time", db_data = toy_dbts, response = "sv",
-#'   perst_tf = c(50, 100), svbl_i = NULL, tbl_i = NULL, bl_data = NULL,
-#'   bl_tf = NULL, na_rm = TRUE
+#'   svdb_i = "stat_var", tdb_i = "time", db_data = toy_dbts, bl = "db",
+#'   bl_tf = c(1, 9), perst_tf = c(50, 100), svbl_i = NULL, tbl_i = NULL, bl_data = NULL,
+#'   na_rm = TRUE
+#' )
+#' persistence(
+#'   svdb_i = "stat_var", tdb_i = "time", db_data = toy_dbts, bl = "bl",
+#'   bl_tf = c(50, 100), perst_tf = c(50, 100), svbl_i = NULL, tbl_i = NULL, bl_data = NULL,
+#'   na_rm = TRUE
 #' )
 #' @export
-persistence <- function(svdb_i, tdb_i, db_data = NULL, response, perst_tf,
+persistence <- function(svdb_i, tdb_i, db_data = NULL, perst_tf, bl, bl_tf,
                         svbl_i = NULL, tbl_i = NULL, bl_data = NULL,
-                        bl_tf = NULL, na_rm = TRUE
+                        na_rm = TRUE
                         ) {
     dbts_df <- format_input(input = "db", svdb_i, tdb_i, db_data)
 
-    response_df <- sort_response(response, dbts_df, svbl_i, tbl_i, bl_data) %>%
-        dplyr::filter(t >= min(perst_tf), t <= max(perst_tf))
+    if (bl == "input") {
+        blts_df <- format_input(input = "bl", svbl_i, tbl_i, bl_data) ##%>%
+            dplyr::rename("t" = tbl_c,
+                          "sv" = svbl_c)
+    } else {
+        if (bl == "db") {
+            if(max(bl_tf) > min(perst_tf)) {
+                stop("Baseline overlaps with persistence period. Check Details.")
+            }
+            blts_df <- dbts_df %>%
+            dplyr::rename("t" = tdb_c,
+                          "sv" = svdb_c)
+        } else {
+            stop("bl must be \"input\" or \"db\".")
+        }
+    }
 
-    persistence <- response_df %>%
-        dplyr::mutate(low_lim = mean(response_df$response, na.rm = na_rm) -
-                          sd(response_df$response, na.rm = na_rm),
-                      high_lim = mean(response_df$response, na.rm = na_rm) +
-                          sd(response_df$response, na.rm = na_rm)) %>%
-        rowwise() %>%
-        dplyr::mutate(persist = all(response >= low_lim, response <= high_lim)) %>%
-        dplyr::group_by(persist) %>%
-        dplyr::summarize(n_p = n()) %>%
+    perst_zone <- blts_df %>%
         dplyr::ungroup() %>%
-        dplyr::mutate(persistence = n_p/sum(n_p)) %>%
+        dplyr::filter(t >= min(bl_tf), t <= max(bl_tf)) %>%
+        dplyr::select(sv) %>%
+        dplyr::summarize(mean_sv = mean(sv, na.rm = na_rm), sd_sv = sd(sv, na.rm = na_rm)) %>%
+        dplyr::summarize(low_lim = mean_sv - sd_sv,
+                         high_lim = mean_sv + sd_sv)
+
+    persistence <- dbts_df %>%
+        dplyr::filter(tdb_c >= min(perst_tf), tdb_c <= max(perst_tf)) %>%
+        dplyr::rowwise() %>%
+        dplyr::mutate(persist = all(svdb_c >= perst_zone$low_lim, svdb_c <= perst_zone$high_lim)) %>%
+        dplyr::group_by(persist) %>%
+        dplyr::summarize(n_p = dplyr::n()) %>%
+        dplyr::ungroup() %>%
         dplyr::filter(persist == TRUE) %>%
+        dplyr::mutate(persistence = n_p/sum(n_p)) %>%
         dplyr::pull(persistence)
     return(persistence)
 }
