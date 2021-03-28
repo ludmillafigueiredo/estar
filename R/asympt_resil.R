78f09882042934bfc11ad48507b4a96e37bc8970
ludmillfaigueiredo <ludmillafi@gmail.com>
2021-03-28 18:33:47 +0200

    Feat: Created function for asymptotic resilience

---
+++ R/asympt_resil.R
@@ -0,0 +1,12 @@
+#' Calculate the asymptotic resilience of a community from its community matrix.
+#' \code{initi_resil} calculates asymptotic resilience as the slowest/long-term asymptotic rate of return to equilibrium after a pulse perturbation (Arnoldi et al. 2016, Downing et al. 2020).
+#'
+#' @param B a matrix, containing the species or functional groups in the community. Can be calculated with \code{\link{get_B}}.
+#'
+#' @return a numeric, the asymptotic resilience
+#'
+#' @export
+asympt_resil <- function(B){
+    l_dom <- eigen(B)$values[1]
+    i_s <- -log(abs(l_dom))
+}
