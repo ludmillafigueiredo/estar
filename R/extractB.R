d31bb21d4a6efeeb386028550e5834620ee0d63a
ludmillfaigueiredo <ludmillafi@gmail.com>
2021-03-28 18:37:04 +0200

    Feat: Created function to extract B from marss results

---
+++ R/extractB.R
@@ -0,0 +1,13 @@
+#' Extract the community matrix (B) estimated by a MARSS model
+#'
+#' @param marss.res MARSS object returned by \code{\link[MARRS]{MARSS}}
+#' @param states_names a string vector containing the names of species/groups for which interactions were estimated
+#'
+#' @return a named matrix
+#'
+#' @export
+extractB <- function(marss_res, states_names){
+  stats::coef(marss_res)$B %>%
+    matrix(nrow = length(states_names), ncol = 5, byrow = FALSE,
+           dimnames = list(states_names, states_names))
+}
