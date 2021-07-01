aecb911edad31816d1e50945409d61ec350814bd
radchukv <radchuk.victoria@gmail.com>
2021-07-01 22:43:50 +0200

    code not general enough?

--- R/extractB.R
+++ R/extractB.R
@@ -8,6 +8,6 @@
 #' @export
 extractB <- function(marss_res, states_names){
   stats::coef(marss_res)$B %>%
-    matrix(nrow = length(states_names), ncol = 5, byrow = FALSE,
+    matrix(nrow = length(states_names), ncol = 5, byrow = FALSE,  ## V: why ncol = 5 here? What if the user has a case with a differnet number of funcitonal groups?
            dimnames = list(states_names, states_names))
 }
