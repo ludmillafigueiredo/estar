d547f4099a317eeb82cf2c199dfb8baf2eee796e
radchukv <radchuk.victoria@gmail.com>
2021-07-05 14:09:07 +0200

    Doc: typos correct

--- R/init_resil.R
+++ R/init_resil.R
@@ -1,8 +1,9 @@
 #' Calculate the initial resilience of a community from its community matrix.
-#'\code{initi_resil} calculates initial resilience is as the initial rate of return to equilibrium (Downing et al. 2020).
+#'\code{initi_resil} calculates initial resilience as the initial rate of return to equilibrium (Downing et al. 2020).
 #' The larger its value, the more stable the system, as its “worst case” initial rate of return to equilibrium is faster (Downing et al. 2020).
 #'
-#' @param B a matrix, containing the species or functional groups in the community. Can be calculated with \code{\link{extractB}}.
+#' @param B a matrix, containing the species or functional groups in the community. Can be calculated with \code{\link{extractB}} from the
+#' fitted MARSS object.
 #'
 #' @return a numeric, the initial resilience
 #'
--- R/reactivity.R
+++ R/reactivity.R
@@ -1,6 +1,6 @@
 #' Calculate the reactivity of a community from its community matrix.
 
-#'\code{reactitvity} calculates reactivity as the initial rate of return to equilibrium (Downing et al. 2020). Following Neubert et al. (1996), it is caluclated as the highest eigenvalue of the Hermitian part of the community matrix $B$.
+#'\code{reactitvity} calculates reactivity as the initial rate of return to equilibrium (Downing et al. 2020). Following Neubert et al. (1996), it is calculated as the largest eigenvalue of the Hermitian part of the community matrix $B$.
 #'
 #' @param B a matrix, containing the species or functional groups in the community. Can be calculated with \code{\link{extractB}}.
 #'
