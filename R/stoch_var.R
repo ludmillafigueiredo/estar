77954e63e17f91726b780dd958e5c23e1c3a9d6d
Ludmilla Figueiredo <ludmillafi@gmail.com>
2024-03-24 20:44:10 +0100

    Feat: Added function to calculate intrinsic stochastic invariability

---
+++ R/stoch_var.R
@@ -0,0 +1,13 @@
+#' Calculate the intrinsic stochastic invariability of a community from its community matrix.
+#'\code{stoch_var} calculates the intrinsic stochastic invariability a theoretical equivalent of the univariate measure of invariability (Arnoldi, Loreau, and Haegeman 2016).
+#'
+#' @param B a matrix, containing the species or functional groups in the community. Can be calculated with \code{\link{extractB}} from the
+#' fitted MARSS object.
+#'
+#' @export
+stoch_var <- function(B){
+  id <- diag(nrow(B))
+  kron_sum <- Matrix::kronecker(B, id) + Matrix::kronecker(id, B)
+  Nu_s <- norm(solve(kron_sum), type ='2')
+  I_s <- 1/2*Nu_s
+}
