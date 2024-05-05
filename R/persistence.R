9e562f875e9a2801f2414da41a40461077b22bcc
Ludmilla Figueiredo <ludmillafi@gmail.com>
2024-05-05 18:12:57 +0200

    Fix: persistence() tested

--- R/persistence.R
+++ R/persistence.R
@@ -1,4 +1,4 @@
-#' Calculate the persistance of a state variable inside a defined interval
+#' Calculate the persistence of a state variable inside a defined interval
 #'
 #' \code{persistence} returns the proportion of time the state
 #' variable remained inside the interval defined by the baseline's
@@ -9,7 +9,7 @@
 #'
 #' @return a double, contained in \[0,1\]
 #'
-#' @details If the baseline is defined by the pre-distrubed values of the
+#' @details If the baseline is defined by the pre-disturbed values of the
 #' state variable in the disturbed system (\code{bl = "db"}), this pre-disturbed
 #' time period used as baseline (\code{bl_tf}) cannot overlap with the time period
 #' for which the persistence is to be calculated (\code{metric_tf}), because of redundancy:
@@ -37,54 +37,60 @@
 #'   bl_data = aquacomm_resps
 #' )
 #' @export
-persistence <- function(svdb_i, tdb_i, db_data = NULL, metric_tf, bl, bl_tf = NULL,
-                        svbl_i = NULL, tbl_i = NULL, bl_data = NULL,
-                        na_rm = TRUE
-                        ) {
+persistence <-
+  function(svdb_i,
+           tdb_i,
+           db_data = NULL,
+           metric_tf,
+           bl,
+           bl_tf = NULL,
+           svbl_i = NULL,
+           tbl_i = NULL,
+           bl_data = NULL,
+           na_rm = TRUE) {
     dbts_df <- format_input(input = "db", svdb_i, tdb_i, db_data)
 
     if (bl == "input") {
-        blts_df <- format_input(input = "bl", svbl_i, tbl_i, bl_data) %>%
-            dplyr::rename("sv" = svbl_i)
+      blts_df <- format_input(input = "bl", svbl_i, tbl_i, bl_data)
+      names(blts_df)[which(names(blts_df) == "svbl_i")] <- "sv"
     } else {
-        if (bl == "db") {
-            if(max(bl_tf) > min(metric_tf)) {
-                stop("Baseline overlaps with persistence period. Check Details.")
-            }
-            blts_df <- dbts_df %>%
-            dplyr::ungroup() %>%
-            dplyr::filter(tdb_i >= min(bl_tf), tdb_i <= max(bl_tf)) %>%
-            dplyr::rename("sv" = svdb_i)
-
-        } else {
-            stop("bl must be \"input\" or \"db\".")
+      if (bl == "db") {
+        if (max(bl_tf) > min(metric_tf)) {
+          stop("Baseline overlaps with persistence period. Check Details.")
         }
+        blts_df <-
+          subset(dbts_df, tdb_i >= min(bl_tf) & tdb_i <= max(bl_tf))
+        names(blts_df)[which(names(blts_df) == "svdb_i")] <- "sv"
+      } else {
+        stop("bl must be \"input\" or \"db\".")
+      }
     }
 
-    perst_zone <- blts_df %>%
-        dplyr::select(sv) %>%
-        dplyr::summarize(mean_sv = mean(sv, na.rm = na_rm),
-                         sd_sv = stats::sd(sv, na.rm = na_rm)) %>%
-        dplyr::summarize(low_lim = mean_sv - sd_sv,
-                         high_lim = mean_sv + sd_sv)
+    perst_zone <- list(
+      mean_sv = mean(blts_df$sv, na.rm = na_rm),
+      sd_sv = sd(blts_df$sv, na.rm = na_rm)
+    )
+    perst_zone$low_lim <- perst_zone$mean_sv - perst_zone$sd_sv
+    perst_zone$high_lim <- perst_zone$mean_sv + perst_zone$sd_sv
 
-    persistence_df <- dbts_df %>%
-        dplyr::filter(tdb_i >= min(metric_tf), tdb_i <= max(metric_tf)) %>%
-        dplyr::rowwise() %>%
-        dplyr::mutate(persist = all(svdb_i >= perst_zone$low_lim,
-                                    svdb_i <= perst_zone$high_lim)) %>%
-        dplyr::group_by(persist) %>%
-        dplyr::summarize(n_p = dplyr::n()) %>%
-        dplyr::ungroup()  %>%
-        dplyr::mutate(persistence = n_p/sum(n_p)) %>%
-        dplyr::filter(persist == TRUE)
+    persistence_df <-
+      subset(dbts_df, tdb_i >= min(metric_tf) & tdb_i <= max(metric_tf))
+    persistence_df$persist <-
+      sapply(persistence_df$svdb_i, function(x)
+        all(x >= perst_zone$low_lim & x <= perst_zone$high_lim))
+    persistence_agg <-
+      aggregate(persistence_df$persist,
+                by = list(persistence_df$persist),
+                FUN = length)
+    colnames(persistence_agg) <- c("persist", "n_p")
+
+    persistence = persistence_agg$n_p[which(persistence_agg$persist== TRUE)]/sum(persistence_agg$n_p)
 
     ## necessary if all persist values are FALSE, and data frame ends up empty
-    if (dim(persistence_df)[1] == 0) {
-        persistence = 0
-        return(persistence)
-    } else {
-        return(persistence_df$persistence)
+    if (nrow(persistence_df) == 0) {
+      persistence <- 0
+
     }
-}
+    return(persistence)
+  }
 
