69a39f58d20c7e064fedd0b82aabc24172c4a488
Cédric Scherer <cedricphilippscherer@gmail.com>
2024-07-11 17:02:13 +0200

    add missing ticks to first example

--- R/resistance.R
+++ R/resistance.R
@@ -37,11 +37,11 @@
 #' @return A double, the resistance of the state variable to disturbance.
 #'
 #' @examples
-# resistance(
-#   vd_i = "statvar_db", td_i = "time", d_data = aquacomm_resps, b = "input",
-#   vb_i = "statvar_bl", tb_i = "time", b_data = aquacomm_resps,
-#   res_mode = "lrr", res_time = "defined", res_t = 12
-# )
+#' resistance(
+#'   vd_i = "statvar_db", td_i = "time", d_data = aquacomm_resps, b = "input",
+#'   vb_i = "statvar_bl", tb_i = "time", b_data = aquacomm_resps,
+#'   res_mode = "lrr", res_time = "defined", res_t = 12
+#' )
 #' resistance(
 #'   vd_i = "statvar_db", td_i = "time", d_data = aquacomm_resps, b = "input",
 #'   vb_i = "statvar_bl", tb_i = "time", b_data = aquacomm_resps,
